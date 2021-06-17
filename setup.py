a35745539c7abedd4ab1974023b33378c3c6af01
brugger <kim.brugger@uib.no>
2021-06-17 14:00:26 +0200

    all the stuff

---
+++ setup.py
@@ -0,0 +1,55 @@
+import os
+from setuptools import setup
+import json
+import glob
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith("~"):
+                continue
+            paths.append(os.path.join(path, filename))
+    print( paths )
+    return paths
+
+def get_version():
+    with open('version.json') as json_file:
+        data = json.load(json_file)
+
+    return "{}.{}.{}".format( data['major'], data['minor'], data['patch'])
+
+def get_requirements():
+
+    file_handle = open('requirements.txt', 'r')
+    data = file_handle.read()
+    file_handle.close()
+
+
+    print( data )
+    return data.split("\n")
+#    return "{}.{}.{}".format( data['major'], data['minor'], data['patch'])
+
+def scripts(directory='bin/*') -> []:
+    return glob.glob( directory )
+
+setup(name='kbr-utils',
+      version= get_version(),
+      description='python utils and tools collection',
+      url='https://github.com/brugger/kbr-tools/',
+      author='Kim Brugger',
+      author_email='kbr@brugger.dk',
+      license='MIT',
+      install_requires=get_requirements(),
+      classifiers=[
+        'License :: MIT License',
+        'Programming Language :: Python :: 3'
+        ],      
+      scripts=scripts(),
+      data_files=[('share/kbr-tools/', package_files('share/'))],
+      include_package_data=True,
+      zip_safe=False)
